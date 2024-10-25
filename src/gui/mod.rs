ca5693e41c038f7bb98854c061fdbfa59e59f801
alec-broadhurst <alecrbroad@gmail.com>
2024-10-25 13:35:13 -0500

    moved Message enum into its own file

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,3 +1,4 @@
 pub mod app;
 pub mod dashboards;
+pub mod message;
 pub mod utils;
--- src/gui/utils.rs
+++ src/gui/utils.rs
@@ -1,27 +1,3 @@
-#[derive(Debug, Clone)]
-pub enum Message {
-    NoOp,
-    SwitchDashboard,
-    UpdateTelemetry,
-    UpdateForzaUI {
-        current_rpm: f32,
-        max_rpm: f32,
-        speed: f32,
-        best_lap: String,
-        current_lap: String,
-        delta: String,
-        gear: i32,
-        accel: f32,
-        brake: f32,
-        position: i32,
-        temp_left_f: f32,
-        temp_right_f: f32,
-        temp_left_r: f32,
-        temp_right_r: f32,
-        lap_number: i32,
-    },
-}
-
 #[derive(Default)]
 pub enum DashboardVarient {
     #[default]
