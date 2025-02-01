f38cc7a27b077a47d3cdc63b3febf6ad84058906
alec-broadhurst <alecrbroad@gmail.com>
2025-01-31 19:17:27 -0600

    changed type from element to container

--- src/gui/dashboards/none_ui.rs
+++ src/gui/dashboards/none_ui.rs
@@ -1,13 +1,12 @@
-use iced::widget::text;
-use iced::Element;
+use iced::widget::{text, Container};
 
 use crate::gui::app::Dashboard;
 use crate::gui::message::Message;
 
-pub fn no_dashboard(dash: &Dashboard) -> Element<Message> {
+pub fn no_dashboard(dash: &Dashboard) -> Container<Message> {
     if dash.get_telemetry().is_none() {
-        Element::new(text!("No telemetry detected!"))
+        Container::new(text!("No telemetry detected!"))
     } else {
-        Element::new(text!("Nothing here"))
+        Container::new(text!("Nothing here"))
     }
 }
